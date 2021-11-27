c8ddbf00a439b186fa8e2c72f2397a85c379828b
John Wells <molekewl2017@gmail.com>
2021-11-26 17:14:47 -0800

    use custom hook to refactor code and make re-usable component for the form component

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,21 +1,96 @@
+import useInput from "../hooks/use-input";
+
 const BasicForm = (props) => {
+  const {
+    value: enteredName,
+    isValid: enteredNameIsValid,
+    hasError: nameInputIsInvalid,
+    valueChangeHandler: nameChangeHandler,
+    inputBlurHandler: nameInputBlurHandler,
+    reset: resetName
+  } = useInput((value) => value.trim() !== "");
+
+  const {
+    value: enteredLast,
+    isValid: enteredLastIsValid,
+    hasError: lastInputIsInvalid,
+    valueChangeHandler: lastChangeHandler,
+    inputBlurHandler: lastInputBlurHandler,
+    reset: resetLast
+  } = useInput((value) => value.trim() !== "");
+
+  const {
+    value: enteredEmail,
+    isValid: enteredEmailIsValid,
+    hasError: emailInputIsInvalid,
+    valueChangeHandler: emailChangeHandler,
+    inputBlurHandler: emailInputBlurHandler,
+    reset: resetEmail
+  } = useInput((value) => value.includes('@'));
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!enteredNameIsValid || !enteredLastIsValid || !enteredEmailIsValid) {
+      return;
+    }
+
+    console.log(enteredName);
+    console.log(enteredLast);
+    console.log(enteredEmail);
+    resetName();
+    resetLast();
+    resetEmail();
+  };
+
+  const nameInputClass = nameInputIsInvalid
+    ? "form-control invalid"
+    : "form-control";
+
+  const lastInputClass = lastInputIsInvalid
+    ? "form-control invalid"
+    : "form-control";
+
+  const emailInputClass = emailInputIsInvalid
+    ? "form-control invalid"
+    : "form-control";
+
   return (
-    <form>
-      <div className='control-group'>
-        <div className='form-control'>
-          <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+    <form onSubmit={handleSubmit}>
+      <div className="control-group">
+        <div className={nameInputClass}>
+          <label htmlFor="name">First Name</label>
+          <input
+            type="text"
+            id="name"
+            value={enteredName}
+            onBlur={nameInputBlurHandler}
+            onChange={nameChangeHandler}
+          />
         </div>
-        <div className='form-control'>
-          <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
+        {nameInputIsInvalid && (
+          <p className="error-text">Name field can not be empty.</p>
+        )}
+        <div className={lastInputClass}>
+          <label htmlFor="last">Last Name</label>
+          <input
+            type="text"
+            id="last"
+            value={enteredLast}
+            onBlur={lastInputBlurHandler}
+            onChange={lastChangeHandler}
+          />
         </div>
+        {lastInputIsInvalid && (
+          <p className="error-text">Last name can not be empty</p>
+        )}
       </div>
-      <div className='form-control'>
-        <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+      <div className={emailInputClass}>
+        <label htmlFor="email">E-Mail Address</label>
+        <input type="email" id="email" value={enteredEmail} onBlur={emailInputBlurHandler} onChange={emailChangeHandler} />
       </div>
-      <div className='form-actions'>
+      {emailInputIsInvalid && <p className='error-text'>Email must include '@'</p>}
+      <div className="form-actions">
         <button>Submit</button>
       </div>
     </form>
